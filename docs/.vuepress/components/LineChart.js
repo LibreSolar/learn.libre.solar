383e43d53e90838868e70de4764a230a0bf2c617
Martin Jaeger <17674105+martinjaeger@users.noreply.github.com>
2019-08-28 08:33:48 +0200

    LineChart component for interactive graphs added

---
+++ docs/.vuepress/components/LineChart.js
@@ -0,0 +1,17 @@
+//
+// Custom LineChart component for live/interactive update of charts
+//
+// Further information: https://vue-chartjs.org/guide/#updating-charts
+//
+
+import { Line, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
+
+export default {
+    extends: Line,
+    mixins: [reactiveProp],
+    props: ['chartData', 'options'],
+    mounted () {
+        this.renderChart(this.chartData, this.options)
+    }
+}
