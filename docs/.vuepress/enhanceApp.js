1bba6f6fe27f3b438afa9f264057dbd51700764d
Leonard Möller <leonard.moeller@live.com>
2020-11-20 13:55:21 +0100

    added vuetify

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,10 +1,12 @@
-import Quasar from 'quasar'
+// register vuetify as a global plugin with vuepress
+// .vuepress/enhanceApp.js
+import Vuetify from 'vuetify'
 
 export default ({
   Vue,      // the version of Vue being used in the VuePress app
   options,  // the options for the root Vue instance
   router,   // the router instance for the app
-  siteData  // site metadata
+  siteData,  // site metadata
 }) => {
-  Vue.use(Quasar)
+  Vue.use(Vuetify)
 }
